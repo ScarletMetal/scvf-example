9c5fd73ab676eae4b7b012c8fe9d183e8c30e875
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-09-13 16:52:49 +0300

    added another pipeline

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from scvf import cv_loop
 from scvf.io import NetworkTableIO
 
 from pipelines.reflectives_pipeline import GripPipeline as ReflectivePipeline
+from pipelines.powercube_pipeline import GripPipeline as PowercubePipeline
 
 nt_io = NetworkTableIO("127.0.0.1", "ImageProcessing")
 
-pipelines = {"reflective": ReflectivePipeline()}
+pipelines = {"reflective": ReflectivePipeline(), "powercube" : PowercubePipeline()}
 
 cv_loop.start(pipelines, output_consumer=nt_io.output_consumer, settings_supplier=nt_io.settings_supplier)
 
